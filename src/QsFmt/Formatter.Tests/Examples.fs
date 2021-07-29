651848e5fa1441322adef4646becae19232fcaac
Scott Carda <sccarda@microsoft.com>
2021-07-29 11:04:33 -0700

    update to new test attributes

--- src/QsFmt/Formatter.Tests/Examples.fs
+++ src/QsFmt/Formatter.Tests/Examples.fs
@@ -115,7 +115,7 @@ let ``Updates Borrowing Statements`` =
     }
 }"""
 
-[<UpdateExample>]
+[<Example(ExampleKind.UpdateExample)>]
 let ``Updates Unit Types`` =
     """namespace Foo {
     operation Bar1() : () {
